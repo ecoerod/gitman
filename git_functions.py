c7d835da4c138fb6260fb921fd08810267c8f747
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2016-11-12 12:00:06 +0000

    [Feature] Allows listing of a different user's repositories

--- git_functions.py
+++ git_functions.py
@@ -1,28 +1,32 @@
 import requests
-import json
 import os
 
 def api_call(method, token=None):
     if token:
         token = {'Authorization': 'token {}'.format(token)}
     data = requests.get('https://api.github.com/{}'.format(method),
-                        headers=token).content
-    return json.loads(data.decode('utf-8'))
+                        headers=token)
+    return data.status_code, data.json()
 
 
-def git_list(token, argv):
-    repos_data = api_call('user/repos', token=token)
-    print('Repositories for user: {}'.format(repos_data[0]['owner']['login']))
-    for repo in repos_data:
-        print('* {} - {}'.format(repo['full_name'], repo['description']))
+def git_list(token, args):
+
+    if args.user:
+        status_code, repos_data = api_call('users/{}/repos'.format(args.user))
+    else:
+        status_code, repos_data = api_call('user/repos', token=token)
+
+    if status_code == 200:
+        print('Repositories for user: {}'.format(repos_data[0]['owner']['login']))
+        for repo in repos_data:
+            print('* {} - {}'.format(repo['full_name'], repo['description']))
+    else:
+        print('User "{}" not found'.format(args.user))
 
 
-def git_setup():
+def git_setup(*args, **kwargs):
     with open(os.path.join(os.environ['HOME'], ".gitman"), "w") as cf:
         print('Please provide your Github OAuth token with "repos" permission.')
         token = input('Github OAuth token: ')
         cf.write(token)
 
-
-def git_help():
-    raise NotImplementedError
--- gitman.py
+++ gitman.py
@@ -6,21 +6,27 @@ from git_functions import git_list, git_setup
 
 
 if not os.path.exists(os.path.join(os.environ['HOME'], '.gitman')):
-    # print("Doesn't exist")
     git_setup()
 else:
     with open(os.path.join(os.environ['HOME'], '.gitman'), 'r') as cf:
         GIT_TOKEN = cf.readline()
 
 
+# Setup main parser
 parser = argparse.ArgumentParser(description='Gitman - Github repository manager')
 subparser = parser.add_subparsers(dest="method")
 
+# 'list' method
 list_parser = subparser.add_parser('list', help="List the repositories of the user.",
                                    description="Gitman - List: list the repositories of the user")
+list_parser.add_argument('-u', '--user',
+                         help="Targets a user in Github. Default: user configured by setup")
 
+# 'setup' method
 setup_parser = subparser.add_parser('setup', help="Setup user credentials.")
 
+
+## MAIN PROGRAM
 args = parser.parse_args()
 
 VALID_ARGS = {
@@ -30,5 +36,5 @@ VALID_ARGS = {
 
 try:
     VALID_ARGS[args.method](GIT_TOKEN, args)
-except:
+except Exception as e:
     parser.print_usage()
