a85c83377370a8e04050c52e09a0d10d73c8f63b
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2016-11-12 12:18:41 +0000

    [Documentation] Added dosctrings

--- git_functions.py
+++ git_functions.py
@@ -2,6 +2,16 @@ import requests
 import os
 
 def api_call(method, token=None):
+    '''Sends an api call to github.com and returns the json contained.
+
+    Args:
+        method (str): Represents the URI to query for data.
+        token (str or None): If provided, provides authorization for queries.
+
+    Returns:
+        tuple: Status code of the query and the json contained in the response.
+    '''
+
     if token:
         token = {'Authorization': 'token {}'.format(token)}
     data = requests.get('https://api.github.com/{}'.format(method),
@@ -10,6 +20,18 @@ def api_call(method, token=None):
 
 
 def git_list(token, args):
+    '''Lists the repositories of a user.
+
+    Args:
+        token (str): To provide authorization for the call if needed.
+        args (argparse.Namespace): For optional arguments.
+
+    Outputs:
+        The list of the repositories belonging to the user to stdin.
+
+    Returns:
+        nothing
+    '''
 
     if args.user:
         status_code, repos_data = api_call('users/{}/repos'.format(args.user))
@@ -21,10 +43,26 @@ def git_list(token, args):
         for repo in repos_data:
             print('* {} - {}'.format(repo['full_name'], repo['description']))
     else:
-        print('User "{}" not found'.format(args.user))
+        print('User "{}" not found or not authorized.'.format(args.user))
 
 
 def git_setup(*args, **kwargs):
+    '''Stores the token provided by the user for future use.
+
+    Args:
+        nothing. *args and **kwargs for simplicity of code in gitman.py
+
+    Inputs:
+        A string from stdout representing the Github OAuth token of the caller.
+        Required at least the "repos" permission, for now.
+
+    Outputs:
+        A file ".gitman" on the $HOME of the user containing this token string.
+
+    TODO:
+        - Maybe allow the user to delete his own repositories if given a valid
+        token.
+    '''
     with open(os.path.join(os.environ['HOME'], ".gitman"), "w") as cf:
         print('Please provide your Github OAuth token with "repos" permission.')
         token = input('Github OAuth token: ')
--- gitman.py
+++ gitman.py
@@ -1,5 +1,25 @@
 #! /usr/bin/env python3
 
+'''Gitman - Github repository manager
+
+Allows users control his own repositories in Github.org and operate on others
+easily.
+
+Methods available:
+    list - lists the repositories of the users
+    setup - creates (or modifies) the Github OAuth token used by the program
+
+Examples:
+    $ python gitman.py list
+    $ python gitman.py list -u Nelthorim
+
+TODO:
+    - Create repository functionality.
+    - Clone repository functionality.
+
+'''
+
+
 import os
 import argparse
 from git_functions import git_list, git_setup
@@ -17,16 +37,16 @@ parser = argparse.ArgumentParser(description='Gitman - Github repository manager
 subparser = parser.add_subparsers(dest="method")
 
 # 'list' method
-list_parser = subparser.add_parser('list', help="List the repositories of the user.",
-                                   description="Gitman - List: list the repositories of the user")
+list_parser = subparser.add_parser('list', help='List the repositories of the user.',
+                                   description='Gitman - List: list the repositories of the user')
 list_parser.add_argument('-u', '--user',
-                         help="Targets a user in Github. Default: user configured by setup")
+                         help='Targets a user in Github. Default: user configured by setup')
 
 # 'setup' method
-setup_parser = subparser.add_parser('setup', help="Setup user credentials.")
+setup_parser = subparser.add_parser('setup', help='Setup user credentials.')
 
 
-## MAIN PROGRAM
+# MAIN PROGRAM
 args = parser.parse_args()
 
 VALID_ARGS = {
